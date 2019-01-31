c12483f049006fb5d204ed4f180a82bd622ed0bf
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-30 17:08:19 -0800

    created new student function

---
+++ hackbright_web.py
@@ -0,0 +1,59 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student"""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    html = render_template("student_info.html", 
+                            first= first,
+                            last= last,
+                            github= github)
+
+    return html
+
+@app.route("/create-student")
+def create_student():
+
+    return render_template("new_student.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first = request.form.get('first')
+    last = request.form.get('last')
+    github = request.form.get('github')
+
+    
+    hackbright.make_new_student(first, last, github)
+    
+    return render_template("successfully_added_student.html", first=first,
+                                                                last= last,
+                                                                github= github)
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
